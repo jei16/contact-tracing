15993ef996efc79e47de80da3578a9ed3f3563f0
Jei16 <jhoncarlodc41603@gmail.com>
2022-06-09 23:11:53 +0800

    eighteenth commit

--- contact-tracing/Form1.cs
+++ contact-tracing/Form1.cs
@@ -25,6 +25,7 @@ namespace contact_tracing
         string runnynosee;
         string losssmelltaste;
         string othersymp;
+        
         public Form1()
         {
             InitializeComponent();
@@ -308,13 +309,13 @@ namespace contact_tracing
 
         private void Othersymptext_TextChanged(object sender, EventArgs e)
         {
-
+            othersymp = Othersymptext.Text;
         }
 
         private void othersymptext_TextChanged(object sender, EventArgs e)
         {
 
-        }
+        } 
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
@@ -328,12 +329,15 @@ namespace contact_tracing
 
         private void Year_ValueChanged(object sender, EventArgs e)
         {
-            if (Month.Text == "February" && Year.Value==1904)
+            int leapyear = 1904| 1908| 1912| 1916| 1920| 1924| 1928| 1932| 1936|  1940| 1944| 1948| 1952| 1956| 1960| 1964| 1968|1972| 1976|1980| 1984| 1988|1992| 1996|2000|2004| 2008| 2012| 2016| 2020;
+            
+
+            if (Month.Text == "February" && Year.Value == leapyear)
             {
                 Day.Minimum = 1;
                 Day.Maximum = 29;
             }
-            else if (Month.Text == "February" && Year.Value != 1904 || 1908)
+            else if (Month.Text == "February" && Year.Value != leapyear)
             {
                 Day.Minimum = 1;
                 Day.Maximum = 28;
